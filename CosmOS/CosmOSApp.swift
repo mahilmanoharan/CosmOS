45b689e58aa1737200febfa286ed6262cc6f15ce
Mahil <mahil@unc.edu>
2025-11-25 21:23:36 -0500

    Initial Commit

---
+++ CosmOS/CosmOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CosmOSApp.swift
+//  CosmOS
+//
+//  Created by Mahil Manoharan on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct CosmOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
