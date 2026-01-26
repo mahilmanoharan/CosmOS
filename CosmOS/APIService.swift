60cbcc520df28a10be97e84c2bf35381ac119954
Mahil <mahil@unc.edu>
2026-01-26 18:35:55 -0500

    updated api service for mars photo feature

--- CosmOS/APIService.swift
+++ CosmOS/APIService.swift
@@ -20,4 +20,21 @@ class APIService {
         let decoder = JSONDecoder()
         return try decoder.decode(APODItem.self, from: data)
     }
+    
+    // get mars photos
+    static func fetchMarsPhoto(date: String?) async throws -> [MarsPhoto] {
+        
+        let urlString = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=\(date)&api_key=DEMO_KEY"
+        
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        let decoder = JSONDecoder()
+        let response = try decoder.decode(MarsRoverModel.self, from: data)
+        return response.photos
+        
+    }
 }
